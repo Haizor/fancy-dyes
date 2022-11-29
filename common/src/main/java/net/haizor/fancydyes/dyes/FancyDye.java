b9ca95ef784568cdf1afff2f887141c0a23c0848
Haizor <ebarnesheart@gmail.com>
2022-11-29 14:09:20 -0500

    Limits on what you can dye & gradle changes

--- common/src/main/java/net/haizor/fancydyes/dyes/FancyDye.java
+++ common/src/main/java/net/haizor/fancydyes/dyes/FancyDye.java
@@ -4,10 +4,10 @@ import net.haizor.fancydyes.FancyDyes;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.*;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
@@ -27,9 +27,21 @@ public interface FancyDye {
     static Item getItem(FancyDye dye) {
         return Registry.ITEM.get(new ResourceLocation(FancyDyes.MOD_ID, FancyDyes.DYES.inverse().get(dye) + "_dye"));
     }
+
     static Item getItem(String dye) {
         return getItem(FancyDyes.DYES.get(dye));
     }
+
+    static boolean isDyeable(ItemStack stack) {
+        return !stack.isEmpty() && !stack.is(DYEABLE_BLACKLIST) && (
+            stack.getItem() instanceof TieredItem ||
+            stack.getItem() instanceof Wearable ||
+            stack.is(DYEABLE)
+        );
+    }
+
+    TagKey<Item> DYEABLE = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(FancyDyes.MOD_ID, "dyeable"));
+    TagKey<Item> DYEABLE_BLACKLIST = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(FancyDyes.MOD_ID, "dyeable_blacklist"));
 
     enum Type {
         BASE,
