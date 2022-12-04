b950548170c841e66f77efe27a0e180e4ffc24e1
Haizor <ebarnesheart@gmail.com>
2022-12-04 11:50:46 -0500

    Small changes * Fixed dyable items having empty tags created on them * Added dyes colors for the rest of the vanilla minecraft dyes * Added unified publishing

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+* Fixed dyable items having empty tags created on them
+* Added dyes colors for the rest of the vanilla minecraft dyes
--- common/src/main/java/net/haizor/fancydyes/dyes/StandardDyeColors.java
+++ common/src/main/java/net/haizor/fancydyes/dyes/StandardDyeColors.java
@@ -1,19 +1,28 @@
 package net.haizor.fancydyes.dyes;
 
+import net.minecraft.world.item.DyeColor;
+
 import java.awt.*;
 
 public class StandardDyeColors {
-    public static final DyeColor[] DYE_COLORS = new DyeColor[] {
-        new DyeColor("white", Color.WHITE),
-        new DyeColor("red", Color.RED),
-        new DyeColor("orange", Color.ORANGE),
-        new DyeColor("yellow", Color.YELLOW),
-        new DyeColor("green", Color.GREEN),
-        new DyeColor("cyan", Color.CYAN),
-        new DyeColor("blue", Color.BLUE),
-        new DyeColor("purple", new Color(0x9200FF)),
-        new DyeColor("pink", new Color(0xF675FF))
+    public static final FancyDyeColor[] DYE_COLORS = new FancyDyeColor[] {
+        new FancyDyeColor("red", Color.RED),
+        new FancyDyeColor("orange", Color.ORANGE),
+        new FancyDyeColor("yellow", Color.YELLOW),
+        new FancyDyeColor("green", new Color(0x00AA00)),
+        new FancyDyeColor("lime", new Color(0x00FF00)),
+        new FancyDyeColor("cyan", new Color(0x3C8DB0)),
+        new FancyDyeColor("light_blue", new Color(0x7098FF)),
+        new FancyDyeColor("blue", Color.BLUE),
+        new FancyDyeColor("purple", new Color(0x7300FF)),
+        new FancyDyeColor("pink", new Color(0xF675FF)),
+        new FancyDyeColor("magenta", new Color(0xFF009A)),
+        new FancyDyeColor("brown", new Color(0x703A1B)),
+        new FancyDyeColor("white", Color.WHITE),
+        new FancyDyeColor("light_gray", new Color(0xB7B7B7)),
+        new FancyDyeColor("gray", new Color(0x595959)),
+        new FancyDyeColor("black", Color.BLACK)
     };
 
-    public record DyeColor(String string, Color color) {}
+    public record FancyDyeColor(String string, Color color) {}
 }
